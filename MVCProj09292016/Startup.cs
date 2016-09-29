6c3916a686d913d31402df4cc28623fa92c9f246
sinhajyoti <sinhajyoti@hotmail.com>
2016-09-29 12:36:52 +0530

    Add project files.

---
+++ MVCProj09292016/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVCProj09292016.Startup))]
+namespace MVCProj09292016
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
