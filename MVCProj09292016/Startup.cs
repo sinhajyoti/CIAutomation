9ae53fe2beb49b4b0d079221ef04ce29cd4895ad
sinhajyoti <sinhajyoti@hotmail.com>
2016-10-04 17:54:34 +0530

    back to original

--- MVCProj09292016/Startup.cs
+++ MVCProj09292016/Startup.cs
@@ -9,7 +9,6 @@ namespace MVCProj09292016
         public void Configuration(IAppBuilder app)
         {
             var newApp = app;
-            //var appobject = app;
             ConfigureAuth(app);///default configuration by Jyoti K. Sinha
         }
     }
