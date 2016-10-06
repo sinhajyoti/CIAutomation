793f6bedffca0ee6b192ec59b0364d2a45908e9a
sinhajyoti <sinhajyoti@hotmail.com>
2016-10-06 14:07:37 +0530

    added dbproject

---
+++ SQLScriptProj/Script09292016.PostDeployment.sql
@@ -0,0 +1,21 @@
+﻿/*
+Post-Deployment Script Template							
+--------------------------------------------------------------------------------------
+ This file contains SQL statements that will be appended to the build script.		
+ Use SQLCMD syntax to include a file in the post-deployment script.			
+ Example:      :r .\myfile.sql								
+ Use SQLCMD syntax to reference a variable in the post-deployment script.		
+ Example:      :setvar TableName MyTable							
+               SELECT * FROM [$(TableName)]					
+--------------------------------------------------------------------------------------
+*/
+set nocount on
+
+if OBJECT_ID('dbo.testTable') is null
+begin
+	create table dbo.testTable(id int identity,testdesc varchar(1000)) 
+end
+else
+begin
+	insert into testTable(testdesc) select 'MVC292016 test'
+end
